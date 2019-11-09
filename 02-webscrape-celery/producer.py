8d6b0e458a629f86c060770b0a3b003b3e4284db
Ryan S. McCoy <18177650+ryansmccoy@users.noreply.github.com>
2019-11-08 23:51:27 -0600

    fixed celery

--- 02-webscrape-celery/producer.py
+++ 02-webscrape-celery/producer.py
@@ -1,31 +1,16 @@
+import time
+from celery import Celery
+import consumer
 
-urls = []
-with open(file, 'r') as f:
-    urls = f.read().splitlines()
-
-
-urls = []
-urls = df['url'].tolist()
-url = 'amqp://gotem:gotem1234@35.185.79.116:5762/gvhost'
-
-params.socket_timeout = 5
-params = pika.URLParameters(url)
-
-
-app = Celery('tasks', broker='amqp://gadmin:gotem1234@amqp://gotem:gotem1234@35.185.79.116:5762/gvhost')
-
-#   using docker for both broker and backend
-#   docker run -d --hostname my-rabbit --name some-rabbit rabbitmq:management
-#   docker pull redis
-
-url = r'http://www.blackrock.com'
+app = Celery('tasks', broker='amqp://localhost//')
 
 def produce():
-    try:
-        scrape.delay(str(url))
-        print(("* Submitted: [{}]".format(url)))
-    except:
-        print(("ERROR ", url))
+    with open(f'urls.txt', 'r') as f:
+        urls = f.read().splitlines()
+
+    for url in urls:
+        consumer.download_url.delay(url)
+        print(f"* Submitted: [{url}]")
 
 produce()
 
@@ -33,26 +18,35 @@ produce()
 
 #   urls.txt    #  example
 
-    #
-    #http://www.blackrock.com
-    #http://www.fidelity.com
-    #http://www.nbim.no
-    #http://www.troweprice.com
-    #http://www.wellington.com
-    #http://www.northerntrust.com
-    #http://www.mfs.com
-    #http://www.jpmorganfunds.com
-    #http://www.us.dimensional.com
-    #http://www.lgim.com
-    #http://www.tiaa-cref.org
-    #http://www.invesco.com
-    #http://www.mcm.com
-    #http://www.geodecapital.com
-    #http://www.columbiamanagement.com
-    #http://www.dodgeandcox.com
-    #http://www.oppenheimerfunds.com
-    #http://www.alliancebernstein.com
-    #http://www.apg.nl
-    #http://www.franklintempleton.com
-    #http://www.jennison.com
-    #http://www.gsam.com
+"""
+http://www.apple.com
+http://www.amazon.com
+http://www.abc.xyz
+http://www.microsoft.com
+http://www.facebook.com
+http://www.alibabagroup.com
+http://www.tencent.com
+http://www.berkshirehathaway.com
+http://www.jpmorganchase.com
+http://www.exxonmobil.com
+http://www.jnj.com
+http://usa.visa.com
+http://www.shell.com
+http://www.samsung.com
+http://www.bankofamerica.com
+http://www.icbc.com.cn
+http://www.wellsfargo.com
+http://corporate.walmart.com
+http://www.nestle.com
+http://www.unitedhealthgroup.com
+http://www.intel.com
+http://www.att.com
+http://www.chevron.com
+http://www.ccb.com
+http://www.homedepot.com
+http://www.pfizer.com
+http://www.verizon.com
+http://www.toyota.co.jp
+http://www.ab-inbev.com
+http://www.mastercard.com
+"""
